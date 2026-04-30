0b5b838932add46c978f056a7bc5aaf857391dd5
eleonora <eleonora.yakovlieva-pavlova@quartsoft.com>
2026-04-30 14:58:52 +0300

    added types for talkr/provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,8 @@ import { HeroUIProvider } from "@heroui/react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { Talkr } from "talkr";
 
 import App from "./App";
-import en from "./locales/en.json";
-import uk from "./locales/uk.json";
 import { store } from "./services/store";
 
 import { ThemeProvider } from "@/providers/themeProvider";
